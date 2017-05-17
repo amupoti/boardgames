8cd436d7d3315e169269a5d804711ae0d4be2b85
Marcel Arrufat <amupoti@gmail.com>
2017-05-17 15:33:46 +0200

    Added assembly plugin to generate jar

---
+++ app/src/main/java/org/amupoti/Main.java
@@ -0,0 +1,14 @@
+package org.amupoti;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+
+    logger.info("App started");
+  }
+}
