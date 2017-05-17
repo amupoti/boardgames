cf95d8703ff4d3b0e688d47cb27cb08163140762
Marcel Arrufat <amupoti@gmail.com>
2017-05-17 14:41:27 +0200

    Added tests and logging

---
+++ app/src/test/java/org/amupoti/SampleTest.java
@@ -0,0 +1,17 @@
+package org.amupoti;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
+
+  @Test
+  public void test() {
+
+    logger.info("Logging info in test");
+
+  }
+}
