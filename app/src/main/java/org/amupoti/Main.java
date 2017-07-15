46ba9f1be4dd69c8741dbc4fcfd0fd814216fb5a
Marcel Arrufat <amupoti@gmail.com>
2017-07-15 18:17:09 +0200

    Fixed back packages name

--- app/src/main/java/org/amupoti/Main.java
+++ app/src/main/java/org/amupoti/Main.java
@@ -1,4 +1,4 @@
-package com.newrelic.technicaltest.contributors;
+package org.amupoti;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- app/src/test/java/org/amupoti/SampleTest.java
+++ app/src/test/java/org/amupoti/SampleTest.java
@@ -1,4 +1,4 @@
-package com.newrelic.technicaltest.contributors;
+package org.amupoti;
 
 import org.junit.Test;
 import org.slf4j.Logger;
